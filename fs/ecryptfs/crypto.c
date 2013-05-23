0e5ed2328bcb27fe5fc61cebac72ff78b0879c14
Alvin Tran <althaitran@gmail.com>
2013-05-23 10:11:05 -0400

    Set default cipher mode when initializing crypt_stat values

--- fs/ecryptfs/crypto.c
+++ fs/ecryptfs/crypto.c
@@ -798,6 +798,7 @@ static void ecryptfs_set_default_crypt_stat_vals(
 						      mount_crypt_stat);
 	ecryptfs_set_default_sizes(crypt_stat);
 	strcpy(crypt_stat->cipher, ECRYPTFS_DEFAULT_CIPHER);
+	strcpy(crypt_stat->cipher_mode, ECRYPTFS_DEFAULT_CIPHER_MODE);
 	crypt_stat->key_size = ECRYPTFS_DEFAULT_KEY_BYTES;
 	crypt_stat->flags &= ~(ECRYPTFS_KEY_VALID);
 	crypt_stat->file_version = ECRYPTFS_FILE_VERSION;
