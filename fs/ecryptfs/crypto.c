661be6a41cdd648ed77924f08f80f311df02f1c8
Alvin Tran <althaitran@gmail.com>
2013-05-23 16:28:40 -0400

    Convert 4-spacing with tabs (for sake of consistency)

--- fs/ecryptfs/crypto.c
+++ fs/ecryptfs/crypto.c
@@ -832,7 +832,7 @@ int ecryptfs_new_file_context(struct inode *ecryptfs_inode)
 	    &ecryptfs_superblock_to_private(
 		    ecryptfs_inode->i_sb)->mount_crypt_stat;
 	int cipher_name_len;
-    int cipher_mode_name_len;
+	int cipher_mode_name_len;
 	int rc = 0;
 
 	ecryptfs_set_default_crypt_stat_vals(crypt_stat, mount_crypt_stat);
@@ -852,12 +852,12 @@ int ecryptfs_new_file_context(struct inode *ecryptfs_inode)
 	       mount_crypt_stat->global_default_cipher_name,
 	       cipher_name_len);
 	crypt_stat->cipher[cipher_name_len] = '\0';
-    cipher_mode_name_len =
-        strlen(mount_crypt_stat->global_default_cipher_mode_name);
-    memcpy(crypt_stat->cipher_mode,
-           mount_crypt_stat->global_default_cipher_mode_name,
-           cipher_mode_name_len);
-    crypt_stat->cipher_mode[cipher_mode_name_len] = '\0';
+	cipher_mode_name_len =
+		strlen(mount_crypt_stat->global_default_cipher_mode_name);
+	memcpy(crypt_stat->cipher_mode,
+		mount_crypt_stat->global_default_cipher_mode_name,
+		cipher_mode_name_len);
+	crypt_stat->cipher_mode[cipher_mode_name_len] = '\0';
 	crypt_stat->key_size =
 		mount_crypt_stat->global_default_cipher_key_size;
 	ecryptfs_generate_new_key(crypt_stat);
